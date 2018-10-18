3ef0a1fee939f4570aa865e176384cd34de04f45
freshNfunky <hallo@felixschaller.com>
2018-10-19 00:34:08 +0200

    Material Highlight works with single Objects
    Recursive Highlight of children not yet working

---
+++ Assets/scripts/RayCaster.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class RayCaster : MonoBehaviour {
+
+	public Text text;
+	Camera cam;
+
+	private GameObject lastObject;
+	private GameObject currentObject;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		//if ( Input.GetMouseButtonDown (0)){ 
+			RaycastHit hit; 
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition); 
+			if ( Physics.Raycast (ray, out hit, 1000.0f)) {
+				Debug.Log (text.text);
+				text.text = "You selected the " + hit.transform.name;
+				//StartCoroutine(ScaleMe(hit.transform));
+				currentObject = hit.transform.gameObject;
+				if (hit.collider.isTrigger) {
+					
+					 // ensure you picked right object
+					
+					if (!currentObject.Equals (lastObject)) { 
+						highlight (currentObject, true);
+						if (lastObject != null)
+							highlight (lastObject, false);
+					}
+					
+				}
+				lastObject = currentObject;	
+				Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+			}
+		//}
+	
+	}
+
+	void highlight(GameObject obj, bool status){
+
+		SwitchMaterial switcher = obj.GetComponent<SwitchMaterial> ();
+		if (switcher != null) {
+			if (status)
+				switcher.OnTriggerEnter (null);
+			else
+				switcher.OnTriggerExit (null);
+		}	 
+	}
+}
---
+++ Assets/scripts/SwitchMaterial.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchMaterial : MonoBehaviour {
+
+	//#pragma strict
+	protected Material mat;
+	public Material HighlightMaterial;
+	public bool change;
+
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (change) {
+		//	GetComponent<Renderer> ().material = HighlightMaterial;
+		//} else {
+		//	GetComponent<Renderer>().material = mat;
+		//}	
+	
+	}
+
+	public void OnTriggerEnter(Collider other) {
+		recursiveSetMaterial (this.gameObject, HighlightMaterial);
+		//mat = HighlightMaterial;
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		
+	}
+
+	public void OnTriggerExit(Collider other) {
+		//GetComponent<Renderer>().material = mat;
+		recursiveSetMaterial (this.gameObject, mat);
+	}
+
+	protected void recursiveSetMaterial(GameObject obj, Material mat)
+	{
+		Renderer meshrender = GetComponent<Renderer>();
+		if (meshrender != null) {
+			meshrender.material = mat; 
+
+			// List gs = new List();
+			// Transform[] ts = gameObject.GetComponentsInChildren<Transform>();
+			// if (ts == null)
+			// 	return;
+			foreach (Transform t in obj.transform) {
+				recursiveSetMaterial(t.gameObject, mat);
+			}
+		}
+	}
+}
